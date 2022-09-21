1cf018daa43b64e2c8f6dbc7425c699ce6285ee3
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-09-21 12:18:50 -0300

    added task page

---
+++ src/pages/tasks/TaskPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TaskListComponent from '../../components/containers/task_list'
+
+const TaskPage = () => {
+  return (
+    <div>
+        <TaskListComponent/>
+    </div>
+  )
+}
+
+export default TaskPage
